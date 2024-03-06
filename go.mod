9ac4a6bf8bf396a551dbdff40a65f4476f042d2f
Talluri Sainath <113941618+sainath123112@users.noreply.github.com>
2024-03-06 12:01:25 -0600

    commiting user service changes

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/sainath/e-commerce-app
 
 go 1.22.0
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/gorm v1.25.7 // indirect
+)
--- user-service/main.go
+++ user-service/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sainath/e-commerce-app/user-service/handler"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/user/login", handler.LoginHandler).Methods("POST")
+	http.Handle("/api", r)
+	log.Println("Listening and serving on port: 8080")
+	http.ListenAndServe(":8080", nil)
+}
